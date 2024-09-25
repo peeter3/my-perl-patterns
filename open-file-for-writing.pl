7309ca083c136cb6d09966757fa6c1bcf8386e49
Peeter Pirn <safe.desk5358@pirn.us>
2024-09-25 01:03:18 -0400

    Added comment on how to simulate a failure for opening the output file. Added a croak in case closing the filehandle fails.

--- open-file-for-writing.pl
+++ open-file-for-writing.pl
@@ -13,7 +13,12 @@ my $output_fh;
 my $output_filename = './open-file-for-writing-out.txt';
 my $encoding = ':encoding(UTF-8)';
 
+# In another PowerShell session, run
+# [System.IO.File]::Open('C:\...\open-file-for-writing-out.txt', 'Open', 'Read', 'None')
+# to cause open() to croak. Close the Powershell session to release
+# the lock.
 open($output_fh, "> $encoding", $output_filename) or
     croak "$PROGRAM_NAME: Couldn't open $output_filename in write-open mode: $OS_ERROR";
 say $output_fh 'ZZZ';
-close($output_fh);
+# I don't know how to cause this to croak.
+close($output_fh) or croak "$PROGRAM_NAME: $output_fh: $OS_ERROR";
